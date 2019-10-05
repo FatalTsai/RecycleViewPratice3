66a103f2874cc0d60bb6905262b271736766023d
fatal_tsai <cheney1003@gmail.com>
2019-10-05 14:14:50 +0800

    first commit

--- app/src/main/java/com/example/recycleviewpratice/MainActivity.java
+++ app/src/main/java/com/example/recycleviewpratice/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.recycleviewpratice;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recycler_view;
     private MyAdapter adapter;
+    private Button btnremove;
     private ArrayList<String> mData = new ArrayList<>();
 
 
@@ -37,9 +42,13 @@ public class MainActivity extends AppCompatActivity {
         // 連結元件
         recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
         // 設置RecyclerView為列表型態
-        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        //recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        recycler_view.setLayoutManager(new GridLayoutManager(this, 4));
+
         // 設置格線
-        recycler_view.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        //recycler_view.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recycler_view.addItemDecoration(
+                new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
 
         // 將資料交給adapter
         adapter = new MyAdapter(mData);
@@ -64,6 +73,31 @@ public class MainActivity extends AppCompatActivity {
             ViewHolder(View itemView) {
                 super(itemView);
                 txtItem = (TextView) itemView.findViewById(R.id.txtItem);
+                btnremove= (Button) itemView.findViewById(R.id.btnRemove);
+
+                itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+
+                        Toast.makeText(view.getContext(),
+                                "click " +getAdapterPosition(), Toast.LENGTH_SHORT).show();
+                        // 新增一個項目
+                        adapter.addItem("New Item");
+                        return false;
+                    }
+                });
+
+
+                btnremove.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // 移除項目，getAdapterPosition為點擊的項目位置
+                        removeItem(getAdapterPosition());
+                    }
+                });
+
+
+
             }
         }
 
@@ -84,6 +118,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             return mData.size();
+        }
+
+
+        // 未更動的程式省略..
+
+        // 新增項目
+        public void addItem(String text) {
+            // 為了示範效果，固定新增在位置3。若要新增在最前面就把3改成0
+            mData.add(3,text);
+            Log.d("insert","insert");
+            notifyItemInserted(3);
+        }
+
+        // 刪除項目
+        public void removeItem(int position){
+            mData.remove(position);
+            notifyItemRemoved(position);
         }
     }
 }
